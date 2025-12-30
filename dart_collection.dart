d85b6b7314f3f91680c5744b07d03ed2feceabcf
munaforreal01 <munajakia2001@gmail.com>
2025-12-30 22:30:56 +0800

    Adding assingment

---
+++ dart_collection.dart
@@ -0,0 +1,21 @@
+//Assignment-1
+void main()
+{
+  Set<int> setA = {1,2,3,4,5};
+  Set<int> setB = {4,5,6,7,8};
+
+  print("Elements of Set A: ");
+
+  for (int item in setA)
+    {
+      print(item);
+    }
+
+      print("Elements of Set B: ");
+      for (int item in setB)
+      {
+        print(item);
+      }
+}
+
+
